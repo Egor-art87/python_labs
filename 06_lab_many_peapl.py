9d6cd3bc615e47d50128f0a1481d8b2e4ad451de
Egor-art87 <artuhegor718@gmail.com>
2025-09-16 11:44:19 +0300

    test

---
+++ 06_lab_many_peapl.py
@@ -0,0 +1,6 @@
+n, o, z = int(input()), 0, 0
+for i in range(n):
+    a = input('in_' + str(i + 1) + ':').split()
+    if (a[-1]) == 'True': o +=1
+    elif (a[-1]) == 'False': z += 1
+print(f'out: {o} {z}')    
---
+++ src/04_minutes_to_hhmm.py
@@ -0,0 +1,5 @@
+m = int(input('Введите минуты: '))
+a = m // 60 
+ost = m % 60
+print(f'Митнуты: {m}')
+print(f'{a}:{ost}')
