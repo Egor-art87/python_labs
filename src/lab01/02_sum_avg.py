893781097306062e06ef0c42eba2826949ce14c1
Egor-art87 <artuhegor718@gmail.com>
2025-11-06 19:23:01 +0300

    lab05

--- src/lab01/02_sum_avg.py
+++
@@ -1,5 +0,0 @@
-a = 3.5
-b = 4.25
-summ = a + b
-avg = (a + b) / 2
-print(f'{summ:.2f} {avg:.2f}')
--- src/lab06/greeting.py
+++
@@ -1,6 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description="Пример CLI")
-parser.add_argument("--name", required=True, help="Имя пользователя")
-args = parser.parse_args()
-print(f"Привет, {args.name}!")
